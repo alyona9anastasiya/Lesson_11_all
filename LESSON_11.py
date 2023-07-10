e2a74c4739f40b806ca33c15358bab651592c436
alyona9anastasiya <45105967+alyona9anastasiya@users.noreply.github.com>
2023-07-10 23:46:01 +0300

    Add files via upload

---
+++ LESSON_11.py
@@ -0,0 +1,22 @@
+import json, requests
+
+url = "http://api.giphy.com/v1/gifs/search"
+
+query_params = input()
+resp = requests.get(
+    url, 
+    params={
+        "q": f"{query_params}",
+        "api_key": "iVK5WCK3L5e970meRCfV7UaSgGRb6dDQ",
+        "limit": 5,
+        "offset": 5,
+        "lang": "en",
+        "rating": "g"
+
+    }
+)
+result = resp.json()['data'][0]['url']
+try:
+  print(f'STATUS_CODE = {resp.status_code}, your GIF - {result}')
+except IndexError:
+  pass
